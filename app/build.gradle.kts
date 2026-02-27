bb9e4dcc5d5e1777cb3b020a886786bf29f8d587
Onekamardin <kamardin199626@mail.ru>
2026-02-27 16:19:12 +0300

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.nmedia"
+        applicationId = "ru.netology.nmedia"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
