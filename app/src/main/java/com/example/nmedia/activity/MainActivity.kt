7b551a95c09840bb9707419493476f0a9a647c3c
Onekamardin <kamardin199626@mail.ru>
2026-03-30 18:54:37 +0300

    update

--- app/src/main/java/com/example/nmedia/activity/MainActivity.kt
+++ app/src/main/java/com/example/nmedia/activity/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.nmedia.R
+import com.example.nmedia.adapter.PostsAdapter
 import com.example.nmedia.databinding.ActivityMainBinding
 import com.example.nmedia.viewmodel.PostViewModel
 
@@ -18,53 +19,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(
-                systemBars.left + v.paddingLeft,
-                systemBars.top + v.paddingTop,
-                systemBars.right + v.paddingRight,
-                systemBars.bottom + v.paddingBottom
-            )
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         val viewModel by viewModels<PostViewModel>()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                author.text = post.author
-                published.text = post.published
-                content.text = post.content
-                tvLikeCount.text = formatCount(post.likes)
-                tvShareCount.text = formatCount(post.shareCount)
-                ivLike.setImageResource(
-                    if (post.likedByMe) {
-                        R.drawable.ic_liked_24
-                    } else R.drawable.ic_heart
-                )
-//                ivShare.setOnClickListener {
-//                    post.shareCount++
-//                    tvShareCount.text = formatCount(post.shareCount)
-//                }
-            }
-        }
-        binding.ivLike.setOnClickListener {
-            viewModel.like()
-        }
-        binding.ivShare.setOnClickListener {
-            viewModel.repost()
+        val adapter = PostsAdapter({ viewModel.likeById(it.id) }, { viewModel.repostById(it.id) })
+        binding.recyclerView.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
         }
     }
-
-    private fun formatCount(count: Int): String = when {
-        count < 1000 -> "$count"
-        count < 10_000 -> "${count / 1000}.${(count % 1000) / 100}K".replace(".0K", "K")
-        count < 1_000_000 -> "${count / 1000}K"
-        count < 10_000_000 -> "${count / 1_000_000}.${(count % 1_000_000) / 100_000}M".replace(
-            ".0M",
-            "M"
-        )
-
-        else -> "${count / 1_000_000}M"
-    }
-
-
 }
--- app/src/main/java/com/example/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/com/example/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -1,34 +1,55 @@
 package com.example.nmedia.repository
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.nmedia.dto.Post
 
 class PostRepositoryInMemoryImpl : PostRepository {
 
-    private var post = Post(
-        1,
-        "Нетология. Университет интернет проф...",
-        "21 мая в 18:36",
-        "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен.",
-        shareCount = 500,
-        likes = 1000,
-        likedByMe = true
+    private var posts = listOf(
+        Post(
+            1,
+            "Нетология. Университет интернет проф...",
+            "21 мая в 18:36",
+            "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен.",
+            shareCount = 999,
+            likes = 1000,
+            likedByMe = true
+        ),
+        Post(
+            2,
+            "Нетология. Университет интернет проф...",
+            "21 мая в 18:36",
+            "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен.",
+            shareCount = 500,
+            likes = 1000,
+            likedByMe = true
+        )
     )
 
-    private val data = MutableLiveData(post)
+    private val data = MutableLiveData(posts)
 
-    override fun get() = data
+    override fun getAll(): LiveData<List<Post>> = data
 
-    override fun like() {
-        post = post.copy(
-            likedByMe = !post.likedByMe,
-            likes = if (post.likedByMe) post.likes - 1 else post.likes + 1
-        )
-        data.value = post
+    override fun likeById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                likedByMe = !it.likedByMe,
+                likes = if (it.likedByMe) it.likes - 1 else it.likes + 1
+            )
+        }
+        data.value = posts
     }
 
-    override fun repost() {
-        post = post.copy(shareCount = post.shareCount + 1)
-        data.value = post
+    override fun repostById(id: Long) {
+        posts = posts.map { post ->
+            if (post.id == id) {
+                post.copy(shareCount = post.shareCount + 1)
+            } else {
+                post
+            }
+        }
+        data.value = posts
     }
+
 }
