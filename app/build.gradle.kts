9df1c6b97ab616fe8c88ef28a9c7b612a8b08d40
Onekamardin <kamardin199626@mail.ru>
2026-06-01 10:44:24 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlin.ksp)
-    kotlin("plugin.serialization") version "2.0.21"
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
